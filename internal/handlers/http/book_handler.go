04541df94f28fc394a60f9b8a9a52d403c120e58
Hasan Mustafa <hasan.mustafa@zacco.com>
2022-02-21 10:50:21 +0100

    implemented methods

--- internal/handlers/http/book_handler.go
+++ internal/handlers/http/book_handler.go
@@ -1,6 +1,14 @@
 package http
 
-import "github.com/hsnkorkmaz/go-hexagonal/internal/core/ports"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/hsnkorkmaz/go-hexagonal/internal/core/domain"
+	"github.com/hsnkorkmaz/go-hexagonal/internal/core/ports"
+)
 
 type BookHandler struct {
 	bookService ports.IBookService
@@ -8,4 +16,86 @@ type BookHandler struct {
 
 func NewBookHandler(bookService ports.IBookService) *BookHandler {
 	return &BookHandler{bookService}
+}
+
+func (b *BookHandler) CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book domain.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = b.bookService.CreateBook(book)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (b *BookHandler) GetBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	book, err := b.bookService.GetBook(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(book)
+}
+
+func (b *BookHandler) GetBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := b.bookService.GetBooks()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(books)
+}
+
+func (b *BookHandler) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var book domain.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = b.bookService.UpdateBook(book)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (b *BookHandler) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = b.bookService.DeleteBook(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
