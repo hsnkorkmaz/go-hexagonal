0ce8d1398cb20e1c8e0c24940c7eaad7023c514c
Hasan Mustafa <hasan.mustafa@zacco.com>
2022-02-21 14:46:02 +0100

    add config

---
+++ config/server/config.go
@@ -0,0 +1,13 @@
+package server
+
+const SQL_SERVER = ""
+const SQL_PORT = ""
+const SQL_USER = ""
+const SQL_PASSWORD = ""
+const SQL_DATABASE = ""
+
+const POSTGRES_SERVER = ""
+const POSTGRES_PORT = ""
+const POSTGRES_USER = ""
+const POSTGRES_PASSWORD = ""
+const POSTGRES_DATABASE = ""
--- config/server/mssql.go
+++
@@ -1,7 +0,0 @@
-package server
-
-const SQL_SERVER = ""
-const SQL_PORT = ""
-const SQL_USER = ""
-const SQL_PASSWORD = ""
-const SQL_DATABASE = ""
