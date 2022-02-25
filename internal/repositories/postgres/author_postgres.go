1099dc3e4c7d9e760985619a52e5a5b978bfa235
Hasan Mustafa <hasansabrimustafa@gmail.com>
2022-02-25 13:51:30 +0100

    add postgres repository

--- internal/repositories/postgres/author_postgres.go
+++ internal/repositories/postgres/author_postgres.go
@@ -1,1 +1,81 @@
 package postgres
+
+import (
+	"database/sql"
+
+	"github.com/hsnkorkmaz/go-hexagonal/internal/core/domain"
+	_ "github.com/lib/pq"
+)
+
+type authorPostgres struct {
+	db *sql.DB
+}
+
+func NewAuthorPostgres(db *sql.DB) *authorPostgres {
+	return &authorPostgres{db}
+}
+
+func (sql *authorPostgres) CreateAuthor(author domain.Author) error {
+	query := `INSERT INTO authors (name) VALUES ($1)`
+	_, err := sql.db.Exec(query, author.Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sql *authorPostgres) GetAuthor(id int64) (domain.Author, error) {
+	query := `SELECT id, name FROM authors WHERE id = $1`
+	rows, err := sql.db.Query(query, id)
+	if err != nil {
+		return domain.Author{}, err
+	}
+	defer rows.Close()
+
+	var author domain.Author
+	for rows.Next() {
+		err := rows.Scan(&author.Id, &author.Name)
+		if err != nil {
+			return domain.Author{}, err
+		}
+	}
+	return author, nil
+}
+
+func (sql *authorPostgres) GetAuthors() ([]domain.Author, error) {
+	query := `SELECT id, name FROM authors`
+	rows, err := sql.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var authors []domain.Author
+	for rows.Next() {
+		var author domain.Author
+		err := rows.Scan(&author.Id, &author.Name)
+		if err != nil {
+			return nil, err
+		}
+		authors = append(authors, author)
+	}
+	return authors, nil
+}
+
+func (sql *authorPostgres) UpdateAuthor(author domain.Author) error {
+	query := `UPDATE authors SET name = $1 WHERE id = $2`
+	_, err := sql.db.Exec(query, author.Name, author.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sql *authorPostgres) DeleteAuthor(id int64) error {
+	query := `DELETE FROM authors WHERE id = $1`
+	_, err := sql.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- internal/repositories/postgres/book_postgres.go
+++ internal/repositories/postgres/book_postgres.go
@@ -1,1 +1,81 @@
 package postgres
+
+import (
+	"database/sql"
+
+	"github.com/hsnkorkmaz/go-hexagonal/internal/core/domain"
+	_ "github.com/lib/pq"
+)
+
+type bookPostgres struct {
+	db *sql.DB
+}
+
+func NewBookPostgres(db *sql.DB) *bookPostgres {
+	return &bookPostgres{db}
+}
+
+func (sql *bookPostgres) CreateBook(book domain.Book) error {
+	query := `INSERT INTO books (name, author_id) VALUES ($1, $2)`
+	_, err := sql.db.Exec(query, book.Name, book.Author.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sql *bookPostgres) GetBook(id int64) (domain.Book, error) {
+	query := `SELECT id, name, author_id FROM books WHERE id = $1`
+	rows, err := sql.db.Query(query, id)
+	if err != nil {
+		return domain.Book{}, err
+	}
+	defer rows.Close()
+
+	var book domain.Book
+	for rows.Next() {
+		err := rows.Scan(&book.Id, &book.Name, &book.Author.Id)
+		if err != nil {
+			return domain.Book{}, err
+		}
+	}
+	return book, nil
+}
+
+func (sql *bookPostgres) GetBooks() ([]domain.Book, error) {
+	query := `SELECT id, name, author_id FROM books`
+	rows, err := sql.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var books []domain.Book
+	for rows.Next() {
+		var book domain.Book
+		err := rows.Scan(&book.Id, &book.Name, &book.Author.Id)
+		if err != nil {
+			return nil, err
+		}
+		books = append(books, book)
+	}
+	return books, nil
+}
+
+func (sql *bookPostgres) UpdateBook(book domain.Book) error {
+	query := `UPDATE books SET name = $1, author_id = $2 WHERE id = $3`
+	_, err := sql.db.Exec(query, book.Name, book.Author.Id, book.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sql *bookPostgres) DeleteBook(id int64) error {
+	query := `DELETE FROM books WHERE id = $1`
+	_, err := sql.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
